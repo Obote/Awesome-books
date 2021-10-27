0d33405134556a58fc38f6dec30bc17bad638e7e
Obote <obotedenis256@gmail.com>
2021-10-27 19:08:20 +0300

    Create a add book function

--- javascript/index.js
+++ javascript/index.js
@@ -1,70 +1,98 @@
-const { constant } = require("async");
+// let library = [];
 
-let library = [];
+// const { disconnect } = require("process");
 
-const booklet = document.querySelector('.book');
-const bookShelf = document.querySelector('#bookShelf');
-let idBook = library.length;
+// const booklet = document.querySelector('.book');
+// const bookShelf = document.querySelector('.bookShelf');
+// let idBook = library.length;
 
-function Book(title, author){
-  this.id = idBook;
+// function Book(title, author) {
+//   this.id = idBook;
+//   this.title = title;
+//   this.author = author;
+
+//   idBook += 1;
+// }
+
+// function ReloadLibrary() {
+//   library = JSON.parse(localStorage.library);
+
+//   bookShelf.innerHTML = '';
+//   bookShelf.appendChild(booklet);
+
+//   for (let i = 0; i < library.length; i += 1) {
+//     // eslint-disable-next-line no-use-before-define
+//     DisplayBook(library[i]);
+//   }
+// }
+
+// function SaveBook(title, author) {
+//   const book = new Book(title, author);
+//   if (!Array.isArray(library)) {
+//     library = [];
+//   }
+//   library.push(book);
+//   localStorage.library = JSON.stringify(library);
+//   // eslint-disable-next-line no-undef
+//   ReloadLibrary();
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function AddBook() {
+//   // eslint-disable-next-line no-restricted-globals
+//   e.preventDefault();
+//   const formAddBook = document.forms.AddBook;
+//   const bookData = new FormData(formAddBook);
+
+//   const bookTitle = bookData.get('title');
+//   const bookAuthor = bookData.get('author');
+
+//   formAddBook.reset();
+
+//   SaveBook(bookTitle, bookAuthor);
+// }
+
+// function DeleteBook(id) {
+//   library = library.filter((book) => book.id !== id);
+
+//   localStorage.library = JSON.stringify(library);
+
+//   ReloadLibrary();
+// }
+
+// function DisplayBook(book) {
+//   const clon = booklet.content.cloneNode(true);
+//   // eslint-disable-next-line prefer-template
+//   clon.querySelectorAll('p')[0].innerHTML = 'BOOK NAME: ' + book.title;
+//   // eslint-disable-next-line prefer-template
+//   clon.querySelectorAll('p')[1].innerHTML = 'AUTHOR NAME: ' + book.author;
+
+//   clon.querySelector('button').addEventListener('click', () => { DeleteBook(book.id); });
+
+//   bookShelf.appendChild(clon);
+// }
+
+// ReloadLibrary();
+
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const btn = document.querySelector('.submitBtn');
+const library = [];
+
+function Book(title, author, id) {
   this.title = title;
   this.author = author;
-
-  idBook += 1;
+  this.id = id;
 }
 
-function reloadLibrary() {
-  library = JSON.parse(localStorage.library);
-
-  bookShelf.innerHTML = '';
-  bookShelf.appendChild(booklet);
-
-  for (let i = 0; i < library.length; i += 1) {
-    displayBook(library[i]);
-  }
-}
-
-function saveBook(title, author){
-  const book = new Book(title, author);
-  if (!Array.isArray(library)){
-    library = [];
-  }
-  library.push(book);
-  localStorage.library = JSON.stringify(library);
-  reloadLibrary();
-}
-
-function addBook(){
+btn.addEventListener('click', (e) => {
   e.preventDefault();
-  const formAddBook = document.forms.addBook;
-  const bookData = new FormData(formAddBook);
-
-  const bookTitle = bookData.get('title');
-  const bookAuthor = bookData.get('author');
-
-  formAddBook.reset();
-
-  saveBook(bookTitle, bookAuthor);
-}
-
-function deleteBook(id) {
-  library = library.filter((book) => book.id !== id);
-
-  localStorage.library = JSON.stringify(library);
-
-  reloadLibrary();
-}
-
-
-function displayBook(book) {
-  const clon = book.content.cloneNode(true);
-  clon.querySelectorAll('p')[0].innerHTML = 'BOOK NAME: '+book.title;
-  clon.querySelectorAll('p')[1].innerHTML = 'AUTHOR NAME: '+book.author;
-
-  clon.querySelector('button').addEventListener('click', () => { deleteBook(book.id); });
-
-  bookShelf.appendChild(clon);
-}
-
-reloadLibrary();
+  const books = new Book(title.value, author.value, library.length);
+  library.push(books);
+  const div = document.querySelector('.bookShelf');
+  div.innerHTML = `<ul class="bookList">
+    <li class="bookTitle">${title.value}</li>
+    <li class="bookAuthor">${author.value}</li>
+    <button class="delete">Delete</button>
+  </ul>`;
+});
