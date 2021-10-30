71ada110e8872a055acb475d1337d6b9442b6437
Obote <obotedenis256@gmail.com>
2021-10-30 12:27:36 +0300

    Add review update

--- javascript/index.js
+++ javascript/index.js
@@ -94,7 +94,6 @@ function dateTime() {
   // eslint-disable-next-line no-undef
   const luxonDate = luxon.DateTime.local().setZone('UTC+3');
   const dateFormat = `${luxonDate.monthLong} ${luxonDate.day}th ${luxonDate.year}, ${luxonDate.hour}:${luxonDate.minute}:${luxonDate.second} `;
-  console.log(dateFormat);
   document.getElementById('timeDiv').innerHTML = `${dateFormat}`;
 }
 dateTime();
